b55c4a69ea4440d08949f41d527418e23d84b931
Ore James <jamesore200@gmail.com>
2024-12-11 13:17:30 -0600

    Updated port

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import { MongoClient } from 'mongodb';
 import cors from 'cors';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const mongoUrl = process.env.MONGODB_URI;
 const client = new MongoClient(mongoUrl);
 
