695fc69eb54df07d196bf59ef9fe37131a0ee67f
Ore James <jamesore200@gmail.com>
2024-12-11 13:57:25 -0600

    Updated environment variable

--- src/server.js
+++ src/server.js
@@ -4,11 +4,10 @@ import cors from 'cors';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
-import { MONGO_URL } from './config.js';
 
 const app = express();
 const port = process.env.PORT || 3000;
-const mongoUrl = MONGO_URL;
+const mongoUrl = process.env.MONGODB_URI;
 const client = new MongoClient(mongoUrl);
 
 
