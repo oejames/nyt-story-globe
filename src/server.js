a52198c605cc79526a87ae760b8c853165c3b321
Ore James <jamesore200@gmail.com>
2024-12-11 13:45:12 -0600

    Updated path to config

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import cors from 'cors';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
-import MONGO_URL from './config.js'
+import MONGO_URL from './config';
 
 const app = express();
 const port = process.env.PORT || 3000;
