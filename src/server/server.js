8d82556a92e2bb7f3c6c8287bc08b9ae5eb4daaa
Ore James <jamesore200@gmail.com>
2024-12-22 03:18:45 -0600

    Updated paths

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import routes from './server/routes.js';
+import routes from './routes.js';
 import { closeDB } from './db.js';
 
 const app = express();
